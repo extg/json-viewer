df804c96c7ef4f0f099af999301b16bc7b6e6f23
Ermakoy <own3d.rus@gmail.com>
2019-01-15 23:11:52 +0300

    chore: bring back popup for build

---
+++ src/popup/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./style/index.scss";
+
+/**
+ * Shared component
+ */
+
+ReactDOM.render(<div />, document.getElementById("root") as HTMLElement);
