d89e3458ebac1a8e348babb196ad644024917dc8
Ermakoy <own3d.rus@gmail.com>
2019-01-17 02:01:20 +0300

    feat: add options page

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,3 @@
-browser.runtime.onMessage.addListener(message => {
-  if (message.type === 'GREETING') {
-    return new Promise(resolve =>
-      setTimeout(() => resolve('Hi! Got your message a second ago.'), 1000),
-    );
-  }
-});
-
-chrome.browserAction.onClicked.addListener(tab => tab.id && chrome.tabs.sendMessage(tab.id, {}));
+chrome.browserAction.onClicked.addListener(tab =>
+  chrome.tabs.sendMessage(tab.id, {})
+);
